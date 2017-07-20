4c8736bde71f124f6690782154e4130e81d6fdb5
Alex Babis <acbabis@gmail.com>
2017-07-20 09:06:12 -0700

    Making reactor program more proactive by only reseting timer when fuel cells move

--- src/demos/nuclear-reactor-controller.js
+++ src/demos/nuclear-reactor-controller.js
@@ -4,10 +4,10 @@ export default () => {
     '   // Reactor takes 200s (12k ticks) to burn a fuel cell\n' +
     '   T > 12000 => 20\n' +
     '20:\n' +
-    '   reset T\n' +
     '   // Set this value based on number of reactors\n' +
     '   steam < 10000 => 30\n' +
-    '   => 10\n' +
+    '   => 20\n' +
     '30: // Arms should grab fuel cells on S = 30\n' +
+    '   reset T\n' +
     '   => 10\n';
 };
