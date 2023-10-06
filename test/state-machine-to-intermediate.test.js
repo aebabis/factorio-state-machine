298ffe5d097d5d4803cb44514ca6c870c000176b
Quentin Ormancey <23664359+louga31@users.noreply.github.com>
2023-10-06 13:01:05 +0200

    Add more tests including the failing case

--- test/state-machine-to-intermediate.test.js
+++ test/state-machine-to-intermediate.test.js
@@ -229,4 +229,232 @@ describe('state-machine-to-intermediate', () => {
 
         expect(result).toEqual(expected);
     });
+
+    test('should convert a parse tree of two-state machine with a mix of labels to equivalent intermediate form', () => {
+        const stateMachine = {
+            timers: [],
+            states: [{
+                state: "start",
+                statements: [{
+                    left: 'X',
+                    right: 1,
+                    operator: '+',
+                    out: 'X'
+                }],
+                transitions: [{
+                    condition: {
+                        left: {
+                            left: 'X',
+                            right: 3,
+                            operator: '%'
+                        },
+                        right: 0,
+                        operator: '=',
+                    },
+                    goto: 100
+                }, {
+                    goto: "start"
+                }]
+            }, {
+                state: 100,
+                statements: [{
+                    left: 'Y',
+                    right: 1,
+                    operator: '+',
+                    out: 'Y'
+                }],
+                transitions: [{
+                    goto: "start"
+                }]
+            }]
+        };
+        const expected = {
+            timers: [],
+            states: [{
+                state: 110,
+                statements: [{
+                    start: 110,
+                    operations: [[{
+                        guard: true
+                    }], [{
+                        left: 'GUARD',
+                        right: 1,
+                        operator: '*',
+                        out: 'signal_X'
+                    }]]
+                }, {
+                    start: 112,
+                    operations: [[{
+                        left: 'signal_X',
+                        right: 3,
+                        operator: '%',
+                        out: 'INT_A'
+                    }], [{
+                        left: 'INT_A',
+                        right: 0,
+                        operator: '=',
+                        countFromInput: false,
+                        out: 'INT_A'
+                    }, {
+                        guard: true
+                    }], [{
+                        left: 'INT_A',
+                        right: 'GUARD',
+                        operator: '*',
+                        out: 'INT_A'
+                    }], [{
+                        branch: 'INT_A',
+                        goto: 100
+                    }]]
+                }, {
+                    start: 116,
+                    operations: [[{
+                        guard: true
+                    }], [{
+                        branch: 'GUARD',
+                        goto: 110
+                    }]]
+                }]
+            }, {
+                state: 100,
+                statements: [{
+                    start: 100,
+                    operations: [[{
+                        guard: true
+                    }], [{
+                        left: 'GUARD',
+                        right: 1,
+                        operator: '*',
+                        out: 'signal_Y'
+                    }]]
+                }, {
+                    start: 102,
+                    operations: [[{
+                        guard: true // TODO: Figure out the best way to share guards
+                    }], [{
+                        branch: 'GUARD',
+                        goto: 110
+                    }]]
+                }]
+            }]
+        };
+
+        const result = stateMachineToIntermediate(stateMachine);
+
+        expect(result).toEqual(expected);
+    });
+
+    test('should convert a parse tree of two-state machine with alphanumerics labels to equivalent intermediate form', () => {
+        const stateMachine = {
+            timers: [],
+            states: [{
+                state: "state1",
+                statements: [{
+                    left: 'X',
+                    right: 1,
+                    operator: '+',
+                    out: 'X'
+                }],
+                transitions: [{
+                    condition: {
+                        left: {
+                            left: 'X',
+                            right: 3,
+                            operator: '%'
+                        },
+                        right: 0,
+                        operator: '=',
+                    },
+                    goto: "state2"
+                }, {
+                    goto: "state1"
+                }]
+            }, {
+                state: "state2",
+                statements: [{
+                    left: 'Y',
+                    right: 1,
+                    operator: '+',
+                    out: 'Y'
+                }],
+                transitions: [{
+                    goto: "state1"
+                }]
+            }]
+        };
+        const expected = {
+            timers: [],
+            states: [{
+                state: 0,
+                statements: [{
+                    start: 0,
+                    operations: [[{
+                        guard: true
+                    }], [{
+                        left: 'GUARD',
+                        right: 1,
+                        operator: '*',
+                        out: 'signal_X'
+                    }]]
+                }, {
+                    start: 2,
+                    operations: [[{
+                        left: 'signal_X',
+                        right: 3,
+                        operator: '%',
+                        out: 'INT_A'
+                    }], [{
+                        left: 'INT_A',
+                        right: 0,
+                        operator: '=',
+                        countFromInput: false,
+                        out: 'INT_A'
+                    }, {
+                        guard: true
+                    }], [{
+                        left: 'INT_A',
+                        right: 'GUARD',
+                        operator: '*',
+                        out: 'INT_A'
+                    }], [{
+                        branch: 'INT_A',
+                        goto: 10
+                    }]]
+                }, {
+                    start: 6,
+                    operations: [[{
+                        guard: true
+                    }], [{
+                        branch: 'GUARD',
+                        goto: 0
+                    }]]
+                }]
+            }, {
+                state: 10,
+                statements: [{
+                    start: 10,
+                    operations: [[{
+                        guard: true
+                    }], [{
+                        left: 'GUARD',
+                        right: 1,
+                        operator: '*',
+                        out: 'signal_Y'
+                    }]]
+                }, {
+                    start: 12,
+                    operations: [[{
+                        guard: true // TODO: Figure out the best way to share guards
+                    }], [{
+                        branch: 'GUARD',
+                        goto: 0
+                    }]]
+                }]
+            }]
+        };
+
+        const result = stateMachineToIntermediate(stateMachine);
+
+        expect(result).toEqual(expected);
+    });
 });
